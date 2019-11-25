4b5dc9a45f984d9001a4a5270d8db20a8ecbd5ab
Manendra Nath Shukla <51334809+manendranathshukla@users.noreply.github.com>
2019-11-26 00:05:39 +0530

    Add files via upload

---
+++ MyThingsWork_Blog/MyThingsWork_Blog/urls.py
@@ -0,0 +1,26 @@
+from django.conf import settings 
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from posts.views import index, blog, post, search,post_delete,post_update,post_create,contact
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+	path('', index),
+	path('blog/', blog,name='post-list'),
+	path('contact/', contact,name='contact'),
+	path('search/',search,name='search'),
+	path('post/<id>/',post,name='post-detail'),
+	path('create/',post_create,name='post-create'),
+	path('post/<id>/update/',post_update,name='post-update'),
+	path('post/<id>/delete/',post_delete,name='post-delete'),
+	path('tinymce/',include('tinymce.urls')),
+	path('accounts/', include('allauth.urls'))
+	
+]
+
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
